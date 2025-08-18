0330c64a22e3684fc9573e0125ae4208a807e5a2
me.mvbernabe@gmail.com <me.mvbernabe@gmail.com>
2025-08-18 09:30:10 +0800

    landing page

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,7 +1,19 @@
 function Home() {
     return(
         <>
-            Landing Page
+            <div className="h-screen bg-green-900 text-white p-5">
+                <h1 className="text-5xl md:text-center">Solar-Drying Reservation System</h1>
+                <div className="flex gap-3 justify-end my-3">
+                    <button className="bg-green-600 rounded-sm font-semibold px-4 py-2">
+                        Register
+                    </button>
+
+                    <button className="bg-green-600 rounded-sm font-semibold px-4 py-2">
+                        Sign In
+                    </button>
+                </div>
+                
+            </div>
         </>
     )
 }
