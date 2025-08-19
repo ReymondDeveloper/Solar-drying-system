69e39efa7d6cf886c02b4b0b9f20d042ca68aaac
me.mvbernabe@gmail.com <me.mvbernabe@gmail.com>
2025-08-19 11:02:20 +0800

    added react-icons

--- FRONTEND/src/views/Registration.jsx
+++ FRONTEND/src/views/Registration.jsx
@@ -3,6 +3,7 @@ import Button from '../component/Button'
 import Loading from '../component/Loading'
 import OTP from '../component/Otp'
 import { useState} from "react";
+import { FaCaretRight } from "react-icons/fa6";
 
 function Registration() {
     const navigate = useNavigate();
@@ -55,35 +56,54 @@ function Registration() {
                         flex flex-col gap-4 md:w-[50%] md:mx-auto
                     '>
                         {formField.map((data, index) => (
-                            <div className='
-                                flex flex-col gap-1 text-white
-                                md:w-full md:mx-auto' 
-                            key={index}>
-                                <label className='font-bold'>{data.label}</label>
-                                {data.type === 'select' ? (
-                                    <select className='outline-0 bg-[rgba(0,80,0,255)] p-2' name={data.name}>
-                                        {data.option?.map((el, idx) => (
-                                            <option key={idx} value={el.value}>
-                                                {el.phrase}
-                                            </option>
-                                        ))}
-                                    </select>
-                                ) : (
-                                    <input
-                                        className='bg-[rgba(0,80,0,255)] p-2 outline-0'
-                                        type={data.type} 
-                                        placeholder={data.placeholder}
-                                        required={data.required}
-                                        spellCheck="false"
-                                        name={data.name}
-                                        minLength={data.min}
-                                        maxLength={data.max}
-                                    />
-                                )}
+                            <div className='flex items-center gap-3'>
+
+                                <div className='flex justify-center items-center gap-2 bg-[rgba(0,0,0,0.5)] w-15 rounded-full px-1'>
+                                    <div className='flex justify-center font-bold text-[#00cc00]'>
+                                        <h1>0{index + 1}</h1>
+                                    </div>
+                                    <FaCaretRight className='text-[#00cc00]' />
+                                </div>
+                                
+                                <div className='
+                                    flex flex-col gap-1 text-white w-full
+                                    md:mx-auto' 
+                                key={index}>
+                                    <label className='font-bold'>{data.label}</label>
+                                    {data.type === 'select' ? (
+                                        <select className='outline-0 bg-[rgba(0,80,0,255)] p-2' name={data.name}>
+                                            {data.option?.map((el, idx) => (
+                                                <option key={idx} value={el.value}>
+                                                    {el.phrase}
+                                                </option>
+                                            ))}
+                                        </select>
+                                    ) : (
+                                        <input
+                                            className='bg-[rgba(0,80,0,255)] p-2 outline-0'
+                                            type={data.type} 
+                                            placeholder={data.placeholder}
+                                            required={data.required}
+                                            spellCheck="false"
+                                            name={data.name}
+                                            minLength={data.min}
+                                            maxLength={data.max}
+                                        />
+                                    )}
+                                </div>
                             </div>
                         ))}
+                        <div className='flex items-center gap-3'>
+                            <div className='flex justify-center items-center gap-2 bg-[rgba(0,0,0,0.5)] w-15 rounded-full px-1'>
+                                <div className='flex justify-center font-bold text-[#00cc00]'>
+                                    <h1>05</h1>
+                                </div>
+                                <FaCaretRight className='text-[#00cc00]' />
+                            </div>
+
+                            <Button type={`submit`} className={`w-full md:mx-auto`}>Create an account</Button>
+                        </div>
                     </div>
-                    <Button type={`submit`} className={`md:w-[50%] md:mx-auto`}>Create an account</Button>
                 </form>
             </div>
         </>
