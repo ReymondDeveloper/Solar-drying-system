45af64534618afcbc23db31d74ef6a13365e3b53
Mhel Voi Bernabe <me.mvbernabe@gmail.com>
2025-08-19 21:45:49 +0800

    Refactor HomeLayout, Header, Navigation, and add Burger component for improved UI and state management

--- FRONTEND/src/component/Navigation.jsx
+++ FRONTEND/src/component/Navigation.jsx
@@ -1,7 +1,137 @@
-function Navigation() {
+import { NavLink } from "react-router-dom";
+import { GiBookmarklet } from "react-icons/gi";
+import {
+  FaMapMarkedAlt,
+  FaFileMedicalAlt,
+  FaGlobe,
+  FaEdit,
+} from "react-icons/fa";
+import { IoPeopleSharp } from "react-icons/io5";
+import { HiClipboardDocumentList } from "react-icons/hi2";
+
+function Navigation({ button, setButton }) {
+  const links = [
+    {
+      role: "admin",
+      navigation: [
+        {
+          to: "/home",
+          title: "Dashboard",
+          icon: <GiBookmarklet className="w-full h-full" />,
+        },
+        {
+          to: "/home/reservations",
+          title: "Reservations",
+          icon: <FaMapMarkedAlt className="w-full h-full" />,
+        },
+        {
+          to: "/home/availability",
+          title: "Availability",
+          icon: <IoPeopleSharp className="w-full h-full" />,
+        },
+        {
+          to: "/home/accounts",
+          title: "Accounts",
+          icon: <HiClipboardDocumentList className="w-full h-full" />,
+        },
+        {
+          to: "/home/reports",
+          title: "Reports",
+          icon: <FaFileMedicalAlt className="w-full h-full" />,
+        },
+      ],
+    },
+    {
+      role: "farmer",
+      navigation: [
+        {
+          to: "/home",
+          title: "Dashboard",
+          icon: <GiBookmarklet className="w-full h-full" />,
+        },
+        {
+          to: "/home/create-reservation",
+          title: "Create Reservation",
+          icon: <FaMapMarkedAlt className="w-full h-full" />,
+        },
+        {
+          to: "/home/reservation-history",
+          title: "Reservation History",
+          icon: <IoPeopleSharp className="w-full h-full" />,
+        },
+        {
+          to: "/home/booking-information",
+          title: "Booking Information",
+          icon: <HiClipboardDocumentList className="w-full h-full" />,
+        },
+        {
+          to: "/home/about",
+          title: "About",
+          icon: <FaGlobe className="w-full h-full" />,
+        },
+      ],
+    },
+    {
+      role: "owner",
+      navigation: [
+        {
+          to: "/home",
+          title: "Dashboard",
+          icon: <GiBookmarklet className="w-full h-full" />,
+        },
+        {
+          to: "/home/update-dryer-status",
+          title: "Update Dryer Status",
+          icon: <FaEdit className="w-full h-full" />,
+        },
+        {
+          to: "/home/booking-requests",
+          title: "Booking Requests",
+          icon: <GiBookmarklet className="w-full h-full" />,
+        },
+        {
+          to: "/home/dryer-information",
+          title: "Dryer Information",
+          icon: <HiClipboardDocumentList className="w-full h-full" />,
+        },
+      ],
+    },
+  ];
   return (
     <>
-      <div className="absolute content-[''] top-[56px] h-full w-10 border"></div>
+      <div
+        className={`
+          z-5 top-[56px] h-[100%] flex flex-col items-center gap-[2px] text-gray-500 font-bold w-[320px] bg-gradient-to-t from-[rgba(0,100,0,255)] via-green-600 to-[rgba(0,100,0,255)] absolute top-0 transition-all duration-500
+          md:bg-none md:bg-[rgba(255,255,255,0.1)] md:relative md:left-0 ${
+            button ? "left-0" : "-left-[100%]"
+          }`}
+      >
+        <div className="text-white w-full p-3 flex items-center justify-center">
+          <b className="text-5xl md:text-center abril-fatface">
+            Solar-Drying Reservation System
+          </b>
+        </div>
+        1
+        {links.map(
+          (link) =>
+            link.role === localStorage.getItem("role") &&
+            link.navigation.map((nav, index) => (
+              <>
+                {nav.icon}
+                <NavLink
+                  key={index}
+                  onClick={() => setButton(false)}
+                  to={nav.to}
+                  className="bg-[rgba(255,255,255,0.2)] w-full flex !ps-5 !py-3 transition-all duration-300 hover:bg-[rgba(255,255,255,0.3)]"
+                >
+                  <h1 className="bg-gradient-to-r from-white to-gray-300 bg-clip-text text-transparent">
+                    {nav.title}
+                  </h1>
+                </NavLink>
+              </>
+            ))
+        )}
+      </div>
     </>
   );
 }
---
+++ FRONTEND/src/component/burger.jsx
@@ -0,0 +1,38 @@
+function Burger({ button, setButton }) {
+  return (
+    <>
+      <button
+        type="button"
+        onClick={() => setButton(!button)}
+        className="w-[25px] h-[25px] md:hidden z-6 left-0"
+      >
+        <div
+          className={`
+                    flex flex-col md:hidden transistion-all
+                    ${button ? "-rotate-135 duration-1000" : "duration-300"}
+                    `}
+        >
+          <span
+            className={`
+                    content-[''] h-[2px] w-[25px] bg-white transition-all
+                    ${button ? "rotate-45 duration-800" : "duration-1000"}
+                    `}
+          ></span>
+          <span
+            className={`
+                    content-[''] h-[2px] w-[25px] bg-white mx-auto! my-2!
+                    ${button ? "hidden" : ""}
+                    `}
+          ></span>
+          <span
+            className={`
+                    content-[''] h-[2px] w-[25px] bg-white transition-all
+                    ${button ? "-rotate-45 duration-300" : "duration-800"}
+                    `}
+          ></span>
+        </div>
+      </button>
+    </>
+  );
+}
+export default Burger;
