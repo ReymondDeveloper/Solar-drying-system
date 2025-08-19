6390ea54a2bb5a0c92debc897a5f2e837c70b467
Reymond Peña <reymond.pena.dev@gmail.com>
2025-08-19 21:36:49 +0800

    add connection db

--- BACKEND/src/server.js
+++ BACKEND/src/server.js
@@ -1,10 +1,23 @@
 const express = require('express');
+const pool = require('./db/postgres.db')
+
 const app = express();
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send("Express Starting...")
 });
 
+app.get("/users", async (req, res) => {
+    try {
+      const result = await pool.query("SELECT * FROM users");
+      res.json(result.rows);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server error");
+    }
+  });
+
 app.listen(4000, () => {
     console.log("listening to port 4000");
-}); 
+});  
