527a0169a4c68a90089e2b6ebb6714f9cb99d6ad
Reymond Peña <reymond.pena.dev@gmail.com>
2025-08-21 22:26:09 +0800

    add update routes

--- BACKEND/src/api/routes/userRoutes.js
+++ BACKEND/src/api/routes/userRoutes.js
@@ -1,11 +1,14 @@
 import express from "express";
-import { getUsers, registerUser, loginUser } from "../controllers/userController.js";
-import { protect } from "../middleware/authMiddleware.js";
+import { getUsers, registerUser, loginUser, updatePassword } from "../controllers/userController.js";
+// import { protect } from "../middleware/authMiddleware.js"; // I comment auth Middleware for the min time
 
 const router = express.Router();
 
-router.get("/", protect, getUsers);   // protected route
+// router.get("/", protect, getUsers);   // protected route
+router.get("/", getUsers);            
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+// router.put("/update-password", protect, updatePassword); // protected route
+router.put("/update/:id", updatePassword);     
 
 export default router;
