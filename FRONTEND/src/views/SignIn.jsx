5aeccff45b8bbc2d4cb1ea2becb17c4f35a243a9
Reymond Peña <reymond.pena.dev@gmail.com>
2025-08-23 13:33:09 +0800

    s

--- FRONTEND/src/views/SignIn.jsx
+++ FRONTEND/src/views/SignIn.jsx
@@ -84,9 +84,7 @@ const handleSubmit = async (e) => {
       name: "password",
       required: true,
       autoComplete: "password",
-      onChange: (e) => setPassword(e.target.value),
-      minLength: 6,
-      maxLength: 16
+      onChange: (e) => setPassword(e.target.value)
     },
   ];
   return (
@@ -120,7 +118,7 @@ const handleSubmit = async (e) => {
                   </div>
                 ))}
 
-                <Button type={`submit`} className={`w-full bg-green-600 hover:bg-green-700 text-white`}>
+                <Button type={`submit`} className={`w-full`}>
                   Sign In
                 </Button>
 
