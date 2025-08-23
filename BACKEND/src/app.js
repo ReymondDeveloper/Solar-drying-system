450d1e87b10cf380dd31d958a25a76a972025c01
Reymond Peña <reymond.pena.dev@gmail.com>
2025-08-23 14:06:17 +0800

    add cors

--- BACKEND/src/app.js
+++ BACKEND/src/app.js
@@ -2,10 +2,12 @@ import express from "express";
 import dotenv from "dotenv"
 import userRoutes from "../src/api/routes/userRoutes.js";
 import { errorHandler } from "../src/api/middleware/errorHandler.js";
+import cors from "cors";
 
 dotenv.config();
 const app = express();
 
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
