ebc00be56d61f4e98ab57c2bdfe41da22c7bb2ed
Mhel Voi A. Bernabe <me.mvbernabe@gmail.com>
2025-11-05 09:32:13 +0800

    Update userController.js

--- BACKEND/src/api/controllers/userController.js
+++ BACKEND/src/api/controllers/userController.js
@@ -1,7 +1,6 @@
 import User from "../models/userModel.js";
 import bcrypt from "bcryptjs";
 import supabase from "../../database/supabase.db.js";
-import { sendOtpEmail } from "../helpers/sendOtpEmail.js";
 import jwt from "jsonwebtoken";
 import axios from "axios";
 
