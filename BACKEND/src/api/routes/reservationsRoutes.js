6845b8f3d90493732d9757c7fa01c1ba9fd3578f
Mhel Voi Bernabe <me.mvbernabe@gmail.com>
2025-12-14 10:50:43 +0800

    Enhance reservation management: add dryer owner filter, update report generation, and improve data handling in Reservations component

--- BACKEND/src/api/routes/reservationsRoutes.js
+++ BACKEND/src/api/routes/reservationsRoutes.js
@@ -7,20 +7,20 @@ import {
   deleteReservation,
   checkReservation,
   getReservationsByOwner,
-  getAllOwnersWithDryers,
-  getArchivedReservations
+  getAllOwnersWithReservation,
+  getArchivedReservations,
 } from "../controllers/ReservationsController.js";
 
 const router = express.Router();
 
 router.get("/", getReservations);
 router.get("/check", checkReservation);
-router.get("/owners", getAllOwnersWithDryers);
+router.get("/owners", getAllOwnersWithReservation);
 router.get("/owner", getReservationsByOwner);
 router.post("/", createReservation);
 router.put("/:id", updateReservation);
 router.delete("/:id", deleteReservation);
-router.get("/home", getReservationById); 
+router.get("/home", getReservationById);
 router.get("/archive", getArchivedReservations);
 
 export default router;
